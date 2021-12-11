96b45b7874d10b991cd245a01ee8eef6b2ea319c
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2021-12-11 12:58:46 -0600

    commit tree 80f70a241bb4b3ca495ed152b1208a84ffe850fb

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
-# Your code here!
+def greet_programmer
+    p "Hello, programmer!"
+end
+
+def greet(name)
+    p "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    p "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    else
+        return number / 2
+    end
+end
